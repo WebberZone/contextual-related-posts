a9cac9f10dd50ad0ba2791f6d453b591789f0548
Ajay D'Souza <me@ajaydsouza.com>
2015-09-26 20:52:19 +0100

    Added explanation on category ID difference

--- admin/main-view.php
+++ admin/main-view.php
@@ -211,8 +211,17 @@ if ( ! defined( 'WPINC' ) ) {
 							<textarea class="wickEnabled:MYCUSTOMFLOATER" cols="50" rows="3" wrap="virtual" name="exclude_cat_slugs"><?php echo ( stripslashes( $crp_settings['exclude_cat_slugs'] ) ); ?></textarea>
 						</div>
 						<p class="description"><?php _e( 'Comma separated list of category slugs. The field above has an autocomplete so simply start typing in the beginning of your category name and it will prompt you with options.', 'contextual-related-posts' ); ?></p>
-						<p class="description highlight"><?php _e( 'Excluded category IDs are:', 'contextual-related-posts' );
-						echo ' ' . $crp_settings['exclude_categories']; ?></p>
+						<p class="description highlight">
+							<?php
+								_e( 'Excluded category IDs are:', 'contextual-related-posts' );
+								echo ' ' . $crp_settings['exclude_categories'];
+							?>
+						</p>
+						<p class="description">
+							<?php
+								_e( 'These might differ from the IDs visible in the Categories page which use the <code>term_id</code>. CRP uses the <code>term_taxonomy_id</code> which is unique to this taxonomy.', 'contextual-related-posts' );
+							?>
+						</p>
 					</td>
 				</tr>
 
