9f013b75c4369e963d66cc0e076a44dd8fb1a6e8
Ajay DSouza <me@ajaydsouza.com>
2017-05-29 21:37:15 +0100

    Updated links code

--- admin/loader.php
+++ admin/loader.php
@@ -14,23 +14,23 @@
  *
  * @version	1.8.10
  *
- * @param	array $links Links array.
- * @return	array	Links array with our settings link added
+ * @param array $links Links array.
+ * @return array Links array with our settings link added
  */
 function crp_plugin_actions_links( $links ) {
 
 	return array_merge( array(
-		'settings' => '<a href="' . admin_url( 'options-general.php?page=crp_options' ) . '">' . __( 'Settings', 'contextual-related-posts' ) . '</a>',
+		'settings' => '<a href="' . admin_url( 'options-general.php?page=crp_options' ) . '">' . esc_html__( 'Settings', 'contextual-related-posts' ) . '</a>',
 	), $links );
 
 }
-add_filter( 'plugin_action_links_' . plugin_basename( plugin_dir_path( __DIR__ ) . 'contextual-related-posts.php' ), 'crp_plugin_actions_links' );
+add_filter( 'plugin_action_links_' . plugin_basename( CRP_PLUGIN_FILE ), 'crp_plugin_actions_links' );
 
 
 /**
  * Add links to the plugin action row.
  *
- * @since	1.4
+ * @since 1.4
  *
  * @param array  $links Links array.
  * @param string $file Plugin file name.
@@ -38,13 +38,15 @@ add_filter( 'plugin_action_links_' . plugin_basename( plugin_dir_path( __DIR__ )
  */
 function crp_plugin_actions( $links, $file ) {
 
-	$plugin = plugin_basename( plugin_dir_path( __DIR__ ) . 'contextual-related-posts.php' );
+	if ( $file === plugin_basename( CRP_PLUGIN_FILE ) ) {
 
-	/**** Add links ****/
-	if ( $file === $plugin ) {
-		$links[] = '<a href="http://wordpress.org/support/plugin/contextual-related-posts">' . __( 'Support', 'contextual-related-posts' ) . '</a>';
-		$links[] = '<a href="https://ajaydsouza.com/donate/">' . __( 'Donate', 'contextual-related-posts' ) . '</a>';
-		$links[] = '<a href="https://github.com/WebberZone/contextual-related-posts">' . __( 'Contribute', 'contextual-related-posts' ) . '</a>';
+		$new_links = array(
+			'support'    => '<a href = "http://wordpress.org/support/plugin/contextual-related-posts">' . esc_html__( 'Support', 'contextual-related-posts' ) . '</a>',
+			'donate'     => '<a href = "https://ajaydsouza.com/donate/">' . esc_html__( 'Donate', 'contextual-related-posts' ) . '</a>',
+			'contribute' => '<a href = "https://github.com/WebberZone/contextual-related-posts">' . esc_html__( 'Contribute', 'contextual-related-posts' ) . '</a>',
+		);
+
+		$links = array_merge( $links, $new_links );
 	}
 	return $links;
 }
