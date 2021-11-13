d0df043f435ffac67e2431725d79a6817f58a1e6
Ajay DSouza <me@ajaydsouza.com>
2021-11-13 14:29:45 +0000

    Don't enqueue wp-editor on widgets.php
    Fixes #152

--- includes/admin/blocks/related-posts.php
+++ includes/admin/blocks/related-posts.php
@@ -47,24 +47,27 @@ function render_crp_block( $attributes ) {
  * @since 2.8.0
  */
 function crp_block_init() {
+	global $pagenow;
 	// Skip block registration if Gutenberg is not enabled/merged.
 	if ( ! function_exists( 'register_block_type' ) ) {
 		return;
 	}
-	$dir = dirname( __FILE__ );
+	$dir         = dirname( __FILE__ );
+	$file_prefix = ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min';
 
-	$index_js = 'related-posts/index.min.js';
+	$dependencies = array( 'wp-element', 'wp-blocks', 'wp-components', 'wp-i18n' );
+
+	if ( in_array( $pagenow, array( 'post.php', 'post-new.php' ), true ) ) {
+		array_push( $dependencies, 'wp-editor', 'wp-edit-post', 'wp-block-editor' );
+	} elseif ( 'widgets.php' === $pagenow ) {
+		array_push( $dependencies, 'wp-edit-widgets' );
+	}
+
+	$index_js = "related-posts/index{$file_prefix}.js";
 	wp_register_script( // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.NotInFooter
 		'related-posts-block-editor',
 		plugins_url( $index_js, __FILE__ ),
-		array(
-			'wp-blocks',
-			'wp-i18n',
-			'wp-element',
-			'wp-components',
-			'wp-block-editor',
-			'wp-editor',
-		),
+		$dependencies,
 		filemtime( "$dir/$index_js" )
 	);
 
