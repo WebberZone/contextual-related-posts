5f07faa9cded9405e72b29912e9aa6bdc2b3519c
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2023-10-29 17:38:27 +0000

    Tagged v3.4.0

--- contextual-related-posts.php
+++ contextual-related-posts.php
@@ -15,7 +15,7 @@
  * Plugin Name: Contextual Related Posts
  * Plugin URI:  https://webberzone.com/plugins/contextual-related-posts/
  * Description: Display related posts on your website or in your feed. Increase reader retention and reduce bounce rates
- * Version:     3.4.0-RC3
+ * Version:     3.4.0
  * Author:      WebberZone
  * Author URI:  https://webberzone.com
  * License:     GPL-2.0+
@@ -36,7 +36,7 @@ if ( ! defined( 'WPINC' ) ) {
  * @since 2.9.3
  */
 if ( ! defined( 'CRP_VERSION' ) ) {
-	define( 'CRP_VERSION', '3.3.4' );
+	define( 'CRP_VERSION', '3.4.0' );
 }
 
 
