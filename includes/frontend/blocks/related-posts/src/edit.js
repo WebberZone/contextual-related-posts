b482b3827780649a4dbc481811187f1742901cfb
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2024-04-09 23:13:09 +0100

    Formatting

--- includes/frontend/blocks/related-posts/src/edit.js
+++ includes/frontend/blocks/related-posts/src/edit.js
@@ -43,8 +43,8 @@ import {
  *
  * @return {WPElement} Element to render.
  */
-export default function Edit({ attributes, setAttributes }) {
-	const editor = select('core/editor');
+export default function Edit( { attributes, setAttributes } ) {
+	const editor = select( 'core/editor' );
 	const postId = editor ? editor.getCurrentPostId() : 0;
 	const {
 		heading,
@@ -62,212 +62,212 @@ export default function Edit({ attributes, setAttributes }) {
 
 	const blockProps = useBlockProps();
 	const toggleHeading = () => {
-		setAttributes({ heading: !heading });
+		setAttributes( { heading: ! heading } );
 	};
-	const onChangeTitle = (newTitle) => {
-		setAttributes({
+	const onChangeTitle = ( newTitle ) => {
+		setAttributes( {
 			title: undefined === newTitle ? '' : newTitle,
-		});
+		} );
 	};
-	const onChangeLimit = (newLimit) => {
-		setAttributes({
+	const onChangeLimit = ( newLimit ) => {
+		setAttributes( {
 			limit: undefined === newLimit ? '' : newLimit,
-		});
+		} );
 	};
-	const onChangeOffset = (newOffset) => {
-		setAttributes({
+	const onChangeOffset = ( newOffset ) => {
+		setAttributes( {
 			offset: undefined === newOffset ? '' : newOffset,
-		});
+		} );
 	};
 	const toggleShowExcerpt = () => {
-		setAttributes({ show_excerpt: !show_excerpt });
+		setAttributes( { show_excerpt: ! show_excerpt } );
 	};
 	const toggleShowAuthor = () => {
-		setAttributes({ show_author: !show_author });
+		setAttributes( { show_author: ! show_author } );
 	};
 	const toggleShowDate = () => {
-		setAttributes({ show_date: !show_date });
+		setAttributes( { show_date: ! show_date } );
 	};
 	const toggleRandomOrder = () => {
-		setAttributes({ random_order: !random_order });
+		setAttributes( { random_order: ! random_order } );
 	};
-	const onChangeOrdering = (newOrdering) => {
-		setAttributes({ ordering: newOrdering });
+	const onChangeOrdering = ( newOrdering ) => {
+		setAttributes( { ordering: newOrdering } );
 	};
-	const onChangeThumbnail = (newThumbnailLoc) => {
-		setAttributes({ post_thumb_op: newThumbnailLoc });
+	const onChangeThumbnail = ( newThumbnailLoc ) => {
+		setAttributes( { post_thumb_op: newThumbnailLoc } );
 	};
-	const onChangeOtherAttributes = (newOtherAttributes) => {
-		setAttributes({
+	const onChangeOtherAttributes = ( newOtherAttributes ) => {
+		setAttributes( {
 			other_attributes:
 				undefined === newOtherAttributes ? '' : newOtherAttributes,
-		});
+		} );
 	};
 
 	return (
 		<>
 			<InspectorControls>
 				<PanelBody
-					title={__(
+					title={ __(
 						'Related Posts Settings',
 						'contextual-related-posts'
-					)}
-					initialOpen={true}
+					) }
+					initialOpen={ true }
 				>
 					<PanelRow>
 						<fieldset>
 							<ToggleControl
-								label={__(
+								label={ __(
 									'Show heading',
 									'contextual-related-posts'
-								)}
+								) }
 								help={
 									heading
 										? __(
-											'Heading displayed',
-											'contextual-related-posts'
-										)
+												'Heading displayed',
+												'contextual-related-posts'
+										  )
 										: __(
-											'No Heading displayed',
-											'contextual-related-posts'
-										)
+												'No Heading displayed',
+												'contextual-related-posts'
+										  )
 								}
-								checked={heading}
-								onChange={toggleHeading}
+								checked={ heading }
+								onChange={ toggleHeading }
 							/>
 						</fieldset>
 					</PanelRow>
-					{heading && (
+					{ heading && (
 						<PanelRow>
 							<TextControl
-								label={__(
+								label={ __(
 									'Heading of posts',
 									'contextual-related-posts'
-								)}
-								value={title}
-								onChange={onChangeTitle}
-								help={__(
+								) }
+								value={ title }
+								onChange={ onChangeTitle }
+								help={ __(
 									'Displayed before the list of the posts as a master heading. HTML allowed.',
 									'contextual-related-posts'
-								)}
+								) }
 							/>
 						</PanelRow>
-					)}
+					) }
 					<PanelRow>
 						<fieldset>
 							<TextControl
-								label={__(
+								label={ __(
 									'Number of posts',
 									'contextual-related-posts'
-								)}
-								value={limit}
-								onChange={onChangeLimit}
-								help={__(
+								) }
+								value={ limit }
+								onChange={ onChangeLimit }
+								help={ __(
 									'Maximum number of posts to display',
 									'contextual-related-posts'
-								)}
+								) }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<TextControl
-								label={__(
+								label={ __(
 									'Offset',
 									'contextual-related-posts'
-								)}
-								value={offset}
-								onChange={onChangeOffset}
-								help={__(
+								) }
+								value={ offset }
+								onChange={ onChangeOffset }
+								help={ __(
 									'Number of posts to skip from the top',
 									'contextual-related-posts'
-								)}
+								) }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<ToggleControl
-								label={__(
+								label={ __(
 									'Show excerpt',
 									'contextual-related-posts'
-								)}
+								) }
 								help={
 									show_excerpt
 										? __(
-											'Excerpt displayed',
-											'contextual-related-posts'
-										)
+												'Excerpt displayed',
+												'contextual-related-posts'
+										  )
 										: __(
-											'No excerpt',
-											'contextual-related-posts'
-										)
+												'No excerpt',
+												'contextual-related-posts'
+										  )
 								}
-								checked={show_excerpt}
-								onChange={toggleShowExcerpt}
+								checked={ show_excerpt }
+								onChange={ toggleShowExcerpt }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<ToggleControl
-								label={__(
+								label={ __(
 									'Show author',
 									'contextual-related-posts'
-								)}
+								) }
 								help={
 									show_author
 										? __(
-											'"by Author Name" displayed',
-											'contextual-related-posts'
-										)
+												'"by Author Name" displayed',
+												'contextual-related-posts'
+										  )
 										: __(
-											'No author displayed',
-											'contextual-related-posts'
-										)
+												'No author displayed',
+												'contextual-related-posts'
+										  )
 								}
-								checked={show_author}
-								onChange={toggleShowAuthor}
+								checked={ show_author }
+								onChange={ toggleShowAuthor }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<ToggleControl
-								label={__(
+								label={ __(
 									'Show date',
 									'contextual-related-posts'
-								)}
+								) }
 								help={
 									show_date
 										? __(
-											'Date of post displayed',
-											'contextual-related-posts'
-										)
+												'Date of post displayed',
+												'contextual-related-posts'
+										  )
 										: __(
-											'Date of post not displayed',
-											'contextual-related-posts'
-										)
+												'Date of post not displayed',
+												'contextual-related-posts'
+										  )
 								}
-								checked={show_date}
-								onChange={toggleShowDate}
+								checked={ show_date }
+								onChange={ toggleShowDate }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<SelectControl
-								label={__(
+								label={ __(
 									'Thumbnail option',
 									'contextual-related-posts'
-								)}
-								value={post_thumb_op}
-								onChange={onChangeThumbnail}
-								help={__(
+								) }
+								value={ post_thumb_op }
+								onChange={ onChangeThumbnail }
+								help={ __(
 									'Location of the post thumbnail',
 									'contextual-related-posts'
-								)}
-								options={[
+								) }
+								options={ [
 									{
 										value: 'inline',
 										label: __(
@@ -296,24 +296,24 @@ export default function Edit({ attributes, setAttributes }) {
 											'contextual-related-posts'
 										),
 									},
-								]}
+								] }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<RadioControl
-								label={__(
+								label={ __(
 									'Order posts',
 									'contextual-related-posts'
-								)}
-								selected={ordering}
-								onChange={onChangeOrdering}
-								help={__(
+								) }
+								selected={ ordering }
+								onChange={ onChangeOrdering }
+								help={ __(
 									'This option directly edits the query',
 									'contextual-related-posts'
-								)}
-								options={[
+								) }
+								options={ [
 									{
 										value: 'relevance',
 										label: __(
@@ -335,73 +335,73 @@ export default function Edit({ attributes, setAttributes }) {
 											'contextual-related-posts'
 										),
 									},
-								]}
+								] }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<ToggleControl
-								label={__(
+								label={ __(
 									'Randomize posts',
 									'contextual-related-posts'
-								)}
+								) }
 								help={
 									random_order
 										? __(
-											'Posts are shuffled on each load',
-											'contextual-related-posts'
-										)
+												'Posts are shuffled on each load',
+												'contextual-related-posts'
+										  )
 										: __(
-											'Posts displayed based on above setting',
-											'contextual-related-posts'
-										)
+												'Posts displayed based on above setting',
+												'contextual-related-posts'
+										  )
 								}
-								checked={random_order}
-								onChange={toggleRandomOrder}
+								checked={ random_order }
+								onChange={ toggleRandomOrder }
 							/>
 						</fieldset>
 					</PanelRow>
 					<PanelRow>
 						<fieldset>
 							<TextareaControl
-								label={__(
+								label={ __(
 									'Other attributes',
 									'contextual-related-posts'
-								)}
-								value={other_attributes}
-								onChange={onChangeOtherAttributes}
-								help={__(
+								) }
+								value={ other_attributes }
+								onChange={ onChangeOtherAttributes }
+								help={ __(
 									'Enter other attributes in a URL-style string-query. e.g. post_types=post,page&link_nofollow=1&exclude_post_ids=5,6',
 									'contextual-related-posts'
-								)}
+								) }
 							/>
 						</fieldset>
 					</PanelRow>
 				</PanelBody>
 			</InspectorControls>
 
-			<div {...blockProps}>
-				{!postId ? (
+			<div { ...blockProps }>
+				{ ! postId ? (
 					<Placeholder
-						icon={'list-view'}
-						label={__(
+						icon={ 'list-view' }
+						label={ __(
 							'Contextual Related Posts',
 							'contextual-related-posts'
-						)}
-						instructions={__(
+						) }
+						instructions={ __(
 							'This is a placeholder for the related posts block. Visit the front end of your site to see the related posts.',
 							'contextual-related-posts'
-						)}
+						) }
 					></Placeholder>
 				) : (
 					<Disabled>
 						<ServerSideRender
 							block="contextual-related-posts/related-posts"
-							attributes={attributes}
+							attributes={ attributes }
 						/>
 					</Disabled>
-				)}
+				) }
 			</div>
 		</>
 	);
