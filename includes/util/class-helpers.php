fdd73f37dba797d017fe93d6d56113105bf6073b
Ajay D'Souza <ajaydsouza@users.noreply.github.com>
2026-03-12 15:47:49 +0530

    Harden sanitize_args class key handling

--- includes/util/class-helpers.php
+++ includes/util/class-helpers.php
@@ -342,7 +342,18 @@ class Helpers {
 	public static function sanitize_args( $args ): array {
 		foreach ( $args as $key => $value ) {
 			if ( is_string( $value ) ) {
-				$args[ $key ] = wp_kses_post( $value );
+				switch ( $key ) {
+					case 'class':
+					case 'className':
+					case 'extra_class':
+						$classes           = explode( ' ', $value );
+						$sanitized_classes = array_map( 'sanitize_html_class', $classes );
+						$args[ $key ]      = implode( ' ', $sanitized_classes );
+						break;
+					default:
+						$args[ $key ] = wp_kses_post( $value );
+						break;
+				}
 			}
 		}
 		return $args;
